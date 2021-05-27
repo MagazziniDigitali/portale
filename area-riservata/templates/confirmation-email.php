9bfd6e74539a1eb0978160c1e6fc8ca779ed418f
ilgiovanefaraone <ilgiovane_faraone@yahoo.com>
2021-05-27 17:22:26 +0200

    bug fix

--- area-riservata/templates/confirmation-email.php
+++ area-riservata/templates/confirmation-email.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = "/area-riservata/istituzione/confirm-email?token=" . $encryptedUuid;
+    $link =    "http://" . $_SERVER['HTTP_HOST'] ."/area-riservata/istituzione/confirm-email?token=" . $encryptedUuid;
     $body = '<h1>Conferma Registrazione</h1>';
     $body .= '<p>Grazie per esserti registrato!</p>';
     $body .= '<p><a href="'. $link . '">Clicca qui</a> per confermare la e-mail inserita</p>';
--- area-riservata/templates/notice-email.php
+++ area-riservata/templates/notice-email.php
@@ -2,6 +2,6 @@
 
     //$link = '/area-riservata/';
     $body = '<h1>Un\'istituzione ha effettuato una pre-registrazione</h1>';
-    $body .= '<p>Accedi all\'<a href="/area-riservata/">aera riservata</a> per approvare la registrazione.</p>';
+    $body .= '<p>Accedi all\'<a href="http://' . $_SERVER['HTTP_HOST'] .'/area-riservata/">aera riservata</a> per approvare la registrazione.</p>';
 
 ?>
